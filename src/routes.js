3398b2ebaa8800d0af59af5c0e50deea5a19596b
alexguimenti <alexguimenti@gmail.com>
2019-03-18 17:52:07 -0300

    add 'require-dir' package

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,10 @@ const routes = express.Router()
 
 const authMiddleware = require('./app/middlewares/auth')
 
-const UserController = require('./app/controllers/UserController')
-const SessionController = require('./app/controllers/SessionController')
+const controllers = require('./app/controllers')
 
-routes.post('/user', UserController.store)
-routes.post('/sessions', SessionController.store)
+routes.post('/user', controllers.UserController.store)
+routes.post('/sessions', controllers.SessionController.store)
 
 routes.get('/teste', authMiddleware, (req, res) => res.json({ ok: true }))
 
